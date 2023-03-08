5eeb7a15a2a97542d69624bc44641b48198cbf82
Hüseyin Akan <hakan@mngkargo.com.tr>
2023-03-09 01:52:50 +0300

    JS Middleware design pattern example added

--- CodeSamples/Design_Patterns/Middleware/HusoMiddleware/HusoPipe.cs
+++ CodeSamples/Design_Patterns/Middleware/HusoMiddleware/HusoPipe.cs
@@ -8,7 +8,7 @@ namespace CodeSamples.Design_Patterns.Middleware.HusoMiddleware
 {
     public abstract class HusoPipe
     {
-        public Action<string> _action;
+        public Action<string> _action;  //Action burda string parametresi alan ve void olan bir metot ama istediğin tiple çalış. Nasıl istiyorsan.
         public HusoPipe(Action<string> action)
         {
             _action = action;
---
+++ CodeSamples/Design_Patterns/_TestPatterns/DepInjectionTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples.Design_Patterns._TestPatterns
+{
+    public class DepInjectionTester : ITester
+    {
+        public void Test()
+        {
+            
+        }
+    }
+}
